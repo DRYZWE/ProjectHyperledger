df56144ba6b689ad7fa985a751e2d01994e5f68f
DRYZWE <137956353+DRYZWE@users.noreply.github.com>
2023-11-05 21:26:35 -0300

    Update createLibraryLog.go

--- cc-tools-demo-main/chaincode/eventtypes/createLibraryLog.go
+++ cc-tools-demo-main/chaincode/eventtypes/createLibraryLog.go
@@ -3,11 +3,11 @@ package eventtypes
 import "github.com/hyperledger-labs/cc-tools/events"
 
 // CreateLibraryLog is a log to be emitted on the CCAPI when a library is created
-var CreateLibraryLog = events.Event{
-	Tag:         "createLibraryLog",
-	Label:       "Create Library Log",
-	Description: "Log of a library creation",
+var CreateStationLog = events.Event{
+	Tag:         "createStationLog",
+	Label:       "Create Station Log",
+	Description: "Log of a Station creation",
 	Type:        events.EventLog,                 // Event funciton is to log on the CCAPI
-	BaseLog:     "New library created",           // BaseLog is a base message to be logged
+	BaseLog:     "New Station created",           // BaseLog is a base message to be logged
 	Receivers:   []string{"$org2MSP", "$orgMSP"}, // Receivers are the MSPs that will receive the event
 }
